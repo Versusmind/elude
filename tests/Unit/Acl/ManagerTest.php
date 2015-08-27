dd38464bc8ffdb49d11d13cdc820dc0fb5826941
Arnaud Lahaxe <lahaxe.arnaud@gmail.com>
2015-08-27 19:14:46 +0200

    manager resources

--- tests/Unit/Acl/ManagerTest.php
+++ tests/Unit/Acl/ManagerTest.php
@@ -1,6 +1,10 @@
 <?php namespace Tests\Unit;
 
 use App\Libraries\Acl\Manager\Manager;
+use App\Libraries\Acl\Repositories\Group;
+use App\Libraries\Acl\Repositories\Permission;
+use App\Libraries\Acl\Repositories\Role;
+use App\Libraries\Acl\Repositories\User;
 use Tests\TestCase;
 
 /**
@@ -13,6 +17,43 @@ abstract class ManagerTest extends TestCase
      */
     protected $repository;
 
+    protected $userRepository;
+
+    protected $permissionRepository;
+
+    protected $roleRepository;
+
+    protected $groupRepository;
+
+    protected $faker;
+
+    protected $user;
+
+    protected $group;
+
+    protected $role;
+
+    protected $roleRevoke;
+
+    protected $permissionFirst;
+
+    protected $permissionSecond;
+
+    /**
+     * PermissionResolverTest constructor.
+     */
+    public function __construct($name = null, array $data = array(), $dataName = '')
+    {
+
+        $this->userRepository       = new User();
+        $this->permissionRepository = new Permission();
+        $this->roleRepository       = new Role();
+        $this->groupRepository      = new Group();
+        $this->faker                = \Faker\Factory::create();
+
+        parent::__construct($name, $data, $dataName);
+    }
+
     /**
      * @return Manager
      */
@@ -23,6 +64,40 @@ abstract class ManagerTest extends TestCase
         $this->repository = $this->getManager();
 
         parent::setUp();
+
+        $this->user = $this->userRepository->create([
+            'username' => $this->faker->userName,
+            'email'    => $this->faker->safeEmail,
+            'password' => $this->faker->password(),
+        ]);
+
+        $this->group = $this->groupRepository->create([
+            'name' => uniqid()
+        ]);
+
+        $this->role = $this->roleRepository->create([
+            'name'   => uniqid(),
+            'filter' => 'A'
+        ]);
+
+        $this->roleRevoke = $this->roleRepository->create([
+            'name'   => uniqid(),
+            'filter' => 'R'
+        ]);
+
+        $this->permissionFirst = $this->permissionRepository->create([
+            'area'        => uniqid(),
+            'permission'  => uniqid(),
+            'action'      => uniqid(),
+            'description' => 'test'
+        ]);
+
+        $this->permissionSecond = $this->permissionRepository->create([
+            'area'        => uniqid(),
+            'permission'  => uniqid(),
+            'action'      => uniqid(),
+            'description' => 'test'
+        ]);
 
         $this->initiate();
     }
