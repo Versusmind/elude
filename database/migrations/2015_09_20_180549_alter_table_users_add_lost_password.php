dd07a579aa006a823f4798ad7f6721bbe068f47c
Arnaud Lahaxe <Arnaud.LAHAXE.ext@boursorama.fr>
2015-09-23 12:17:52 +0200

    Set local

--- database/migrations/2015_09_20_180549_alter_table_users_add_lost_password.php
+++ database/migrations/2015_09_20_180549_alter_table_users_add_lost_password.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableUsersAddLostPasswordToken extends Migration
+class AlterTableUsersAddLostPassword extends Migration
 {
     /**
      * Run the migrations.
