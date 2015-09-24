d180cd0e8199d61258480d91c373310cd9c5c436
Benoit Zohar <benoit.zohar@versusmind.eu>
2015-09-24 13:59:24 +0200

    fixed meta configurations

--- resources/assets/app/config.js
+++ resources/assets/app/config.js
@@ -7,6 +7,8 @@ angular.module('app.config', [])
     .constant('version', 'v0.1.0')
     
     .constant('appdir', 'assets/app')
+    
+    .constant('apiUrl', '/api/v1') //you can set the whole URL here : e.g http://example.com/api/v1
 
     .config(function($urlRouterProvider) {
 
